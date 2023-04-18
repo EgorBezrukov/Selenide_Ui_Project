c3611f1d628ead8f80556acb69deb5c353d4bd71
EgorBezrukov <poladez@yandex.ru>
2023-04-18 23:03:54 +0300

    Allure
    Добавил зависимости для формирования Allure отчета

--- src/test/java/ru/egor/qa/uitest/steps/BaseTest.java
+++ src/test/java/ru/egor/qa/uitest/steps/BaseTest.java
@@ -2,25 +2,37 @@ package ru.egor.qa.uitest.steps;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import ru.egor.qa.uitest.common_steps.SwagLabsBaseTest;
 
 abstract public class BaseTest {
 
-    public void setUp() {
+    public static void setUp() {
         Configuration.browser = "chrome";
         Configuration.driverManagerEnabled = true;
         Configuration.browserSize = "1920x1080";
         Configuration.timeout = 60000;
+        SelenideLogger
+                .addListener("AllureSelenide", new AllureSelenide()
+                .screenshots(true)
+                .savePageSource(true)
+        );
     }
-    @BeforeEach
-    public void init(){
+
+    @BeforeAll
+    public static void init() {
         setUp();
     }
 
     @AfterEach
-    public void tearDown(){
-        Selenide.closeWebDriver();
+    public void tearDown() {
+        WebDriverRunner.getWebDriver().close();
+        WebDriverRunner.getWebDriver().quit();
     }
+
+
 }
